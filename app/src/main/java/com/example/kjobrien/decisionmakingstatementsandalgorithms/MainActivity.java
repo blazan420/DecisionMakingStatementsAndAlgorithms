5b922bc3061077bb9cf380fb04c9c87a42af6d59
kevin <kevinjobrien420@gmail.com>
2018-06-08 18:47:45 -0500

    #155 App Number 9 - Part3 - More about decision making statements and algorithms //Complete

--- app/src/main/java/com/example/kjobrien/decisionmakingstatementsandalgorithms/MainActivity.java
+++ app/src/main/java/com/example/kjobrien/decisionmakingstatementsandalgorithms/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.kjobrien.decisionmakingstatementsandalgorithms;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,6 +11,8 @@ import android.widget.TextView;
 import java.security.SecureRandom;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean firstTime = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +24,8 @@ public class MainActivity extends AppCompatActivity {
         final EditText edtNum3 = (EditText)findViewById(R.id.edtNum3);
         Button btnMaximumValue = (Button)findViewById(R.id.btnMaximumValue);
         final TextView txtResult = (TextView)findViewById(R.id.txtResult);
-        Button btnSecureRandomNumbers = (Button)findViewById(R.id.btnSecureRandomNumbers);
-        TextView txtSecureRandomNumbers = (TextView)findViewById(R.id.txtSecureRandomNumbers);
+        final Button btnSecureRandomNumbers = (Button)findViewById(R.id.btnSecureRandomNumbers);
+        final TextView txtSecureRandomNumbers = (TextView)findViewById(R.id.txtSecureRandomNumbers);
 
         btnMaximumValue.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,8 +45,21 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                if(firstTime) {
+                    txtSecureRandomNumbers.setText("");
+                    firstTime = false;
+                }
+
                 SecureRandom secureRandomNumber = new SecureRandom();
-                if
+
+                String oldTextSecureNumbersValue = txtSecureRandomNumbers.getText().toString();
+                if (txtSecureRandomNumbers.getText().toString().length() >=9) {
+                    btnSecureRandomNumbers.setVisibility(View.INVISIBLE);
+                }
+
+                int randomSecureNumber = 1 + secureRandomNumber.nextInt(5);
+                Log.i("LOG", randomSecureNumber + "");
+                txtSecureRandomNumbers.setText(randomSecureNumber + "");
 
             }
         });
