b58028c3a4afc9d80b0ad142c7eb8ad97f4ca8fd
kevin <kevinjobrien420@gmail.com>
2018-06-08 18:24:34 -0500

    #154 App Number 9 - Part2 - More about decision making statements and algorithms //Complete

--- app/src/main/java/com/example/kjobrien/decisionmakingstatementsandalgorithms/MainActivity.java
+++ app/src/main/java/com/example/kjobrien/decisionmakingstatementsandalgorithms/MainActivity.java
@@ -7,6 +7,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.security.SecureRandom;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,9 +18,9 @@ public class MainActivity extends AppCompatActivity {
 
         final EditText edtNum1 = (EditText)findViewById(R.id.edtNum1);
         final EditText edtNum2 = (EditText)findViewById(R.id.edtNum2);
-        EditText edtNum3 = (EditText)findViewById(R.id.edtNum3);
+        final EditText edtNum3 = (EditText)findViewById(R.id.edtNum3);
         Button btnMaximumValue = (Button)findViewById(R.id.btnMaximumValue);
-        TextView txtResult = (TextView)findViewById(R.id.txtResult);
+        final TextView txtResult = (TextView)findViewById(R.id.txtResult);
         Button btnSecureRandomNumbers = (Button)findViewById(R.id.btnSecureRandomNumbers);
         TextView txtSecureRandomNumbers = (TextView)findViewById(R.id.txtSecureRandomNumbers);
 
@@ -27,9 +29,37 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 double num1 = Double.parseDouble(edtNum1.getText().toString());
-                double num2 = Double.parseDouble(edtNum2.getText().toString();)
+                double num2 = Double.parseDouble(edtNum2.getText().toString());
+                double num3 = Double.parseDouble(edtNum3.getText().toString());
+
+                double result = getTheMaximum(num1, num2, num3);
+                txtResult.setText(result + "");
 
             }
         });
+
+        btnSecureRandomNumbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                SecureRandom secureRandomNumber = new SecureRandom();
+                if
+
+            }
+        });
+    }
+
+    public double getTheMaximum(double num1, double num2, double num3) {
+
+        double theMaximumValue = num1;
+        if (num2 > theMaximumValue) {
+            theMaximumValue = num2;
+        }
+        if (num3 > theMaximumValue) {
+            theMaximumValue = num3;
+        }
+
+        return  theMaximumValue;
+
     }
 }
